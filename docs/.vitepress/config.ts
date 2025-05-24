05b57654477d9ce175cf03ea414eeab6e41afea0
Richard Ikegbulam <malpacon101@yahoo.co.uk>
2025-05-24 12:30:19 +0100

    fix: update navigation link for consistency in documentation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
+      { text: "Getting Started", link: "/getting-started" },
     ],
 
     sidebar: [
